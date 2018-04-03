5bca7975260c12f05a94dcbc18c483fb8a787d67
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2018-04-02 20:52:53 -0700

    working on implementing isaac data collection for zkillboard

--- src/main/resources/sites/zkillboard/page.js
+++ src/main/resources/sites/zkillboard/page.js
@@ -8,34 +8,52 @@ $("head").append(
 
 
 //help expression to easily find fields equaling title
-$.expr[':'].textEquals = $.expr.createPseudo(function(arg) {
-    return function( elem ) {
+$.expr[':'].textEquals = $.expr.createPseudo(function (arg) {
+    return function (elem) {
         return $(elem).text().match("^" + arg + "$");
     };
 });
+
+issacDataCollection();
+
+
+/**
+ * function utilized for item data collection
+ */
+function issacDataCollection() {
+
+//helper functions
+
+    function upvoteItem(item) {
+        console.log(item);
+
+    }
+
+    function downvoteItem(item) {
+        console.log(item);
+    }
+
+//do the data collection here
 
 //1. ensure we are on the right page
 
 //if this is the case add headers to row for isaac
 
 //extract the name of the ship
-$("tr td").find("a[href*='/ship/']");
-//this section is to registers the hooks to leave reviews
-$("#DataTables_Table_0 ").find("td.item_dropped, td.item_destroyed").each(function(){
+    $("tr td").find("a[href*='/ship/']");
 
-  //extract the url of the module and the description
+    $("#DataTables_Table_0 ").find("td.item_dropped, td.item_destroyed").each(function () {
 
-  //add isaac buttons at the last row
+        //add isaac buttons at the last row
+        var plusResponse = "<a ship='' item='' onclick='upvoteItem($(this));'><i class='fas fa-plus-circle'  aria-hidden='true' data-toggle='tooltip' title='module is a good choice?' style='color: darkgreen; padding-left: 5px; padding-right: 5px'></i></a>";
+        var minusResponse = "<a ship='' item='' onclick='downvoteItem($(this));'><i class='fas fa-minus-circle'  aria-hidden='true' data-toggle='tooltip' title='module is a bad choice?' style='color: darkred; padding-left: 5px; padding-right: 5px'></i></a>";
 
-    var itemId = "your momma";
-    var plusResponse = "<i onclick='alert(itemId);' class='fas fa-plus-circle'  aria-hidden='true' data-toggle='tooltip' title='module is a good choice?' style='color: darkgreen; padding-left: 5px; padding-right: 5px'></i>";
-    var minusResponse = "<i onclick='alert(itemId);' class='fas fa-minus-circle'  aria-hidden='true' data-toggle='tooltip' title='module is a bad choice?' style='color: darkred; padding-left: 5px; padding-right: 5px'></i>";
+        //+ to approve, with tooltip describing what we do
+        $(this).parent().find("td:last").after('<td>' + plusResponse + minusResponse + '</td>');
+        //- to reject, with tooltip describing what we do
+    });
 
-    //+ to approve, with tooltip describing what we do
-    $(this).parent().find("td:last").after('<td>'+plusResponse+minusResponse+'</td>');
-  //- to reject, with tooltip describing what we do
-});
-
+}
 
 //this sections calculates real world isk to dollar pricing
 
