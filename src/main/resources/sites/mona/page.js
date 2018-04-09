277b282f0e7a8db207487d3b0eacc94befeabcfd
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2018-04-08 20:19:41 -0700

    slight design adjustments

--- src/main/resources/sites/mona/page.js
+++ src/main/resources/sites/mona/page.js
@@ -3,9 +3,7 @@
 //1. ensure we are on the right page, so we don't pollute the scopes
 if (window.location.href.indexOf('mona.fiehnlab.ucdavis.edu') > 0) {
 
-    $("head").append("<link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.0.9/css/all.css\" integrity=\"sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1\" crossorigin=\"anonymous\">");
-
-    $(document).ready(function () {
+    $(document).ready(function () {git
         issacDataCollection();
     });
 
@@ -38,8 +36,8 @@ if (window.location.href.indexOf('mona.fiehnlab.ucdavis.edu') > 0) {
             var encoded = btoa(JSON.stringify(contribution));
 
             //add isaac buttons at the last row
-            var plusResponse = "<span data-item='" + encoded + "' onclick='upvoteItem(atob($(this).data(\"item\")));$(this).parent().parent().fadeOut();'><i class='fas fa-plus-circle'  aria-hidden='true' data-toggle='tooltip' title='this value is correct?' style='color: darkgreen; padding-left: 5px; padding-right: 5px'></i></span>";
-            var minusResponse = "<span data-item='" + encoded + "' onclick='downvoteItem(atob($(this).data(\"item\")));$(this).parent().parent().fadeOut();'><i class='fas fa-minus-circle'  aria-hidden='true' style='color: darkred; padding-left: 5px; padding-right: 5px'></i></span>";
+            var plusResponse = "<span data-item='" + encoded + "' onclick='upvoteItem(atob($(this).data(\"item\")));$(this).parent().parent().fadeOut();'><i class='fa fa-plus-circle'  aria-hidden='true' data-toggle='tooltip' title='this value is correct?' style='color: darkgreen; padding-left: 5px; padding-right: 5px'></i></span>";
+            var minusResponse = "<span data-item='" + encoded + "' onclick='downvoteItem(atob($(this).data(\"item\")));$(this).parent().parent().fadeOut();'><i class='fa fa-minus-circle'  aria-hidden='true' style='color: darkred; padding-left: 5px; padding-right: 5px'></i></span>";
 
             $(this).parent().children("td:eq(0)").removeClass("col-lg-4").removeClass("col-md-4").addClass("col-lg-3").addClass("col-md-3");
             $(this).parent().children("td:eq(1)").removeClass("col-lg-8").removeClass("col-md-8").addClass("col-lg-6").addClass("col-md-6");
